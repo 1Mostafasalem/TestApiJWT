12b8280fc9cb1b44b22c2da03270f38810a75b5d
Mostafa Salem <mostafasalem858@outlook.com>
2021-11-05 03:28:21 +0200

    - Registration EndPoint Part

--- TwstApiJWT/Services/IAuthService.cs
+++ TwstApiJWT/Services/IAuthService.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TestApiJWT.Models;
 
 namespace TestApiJWT.Services
 {
     public interface IAuthService
     {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
     }
 }
